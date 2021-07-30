d56dd60b01a25a81e560dfc43c71c5ed11ee6d5b
Dominic2500 <bscmks435319@gmail.com>
2021-07-30 21:04:54 +0300

    changs done

---
+++ app/src/main/java/com/dan/naari/Maps_Activity.java
@@ -0,0 +1,16 @@
+package com.dan.naari;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+public class Maps_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+    }
+}
